fb06e4e7dfb409e0ff4f8116812eabe8bc3bd68f
shdewz <shdewzofficial@gmail.com>
2020-05-12 01:38:35 +0300

    Slight update

--- js/suijipickems.js
+++ js/suijipickems.js
@@ -1,13 +1,38 @@
-function randomize() {
-    for (var i = 0; i < 16; i++) {
-        var score1 = Math.floor(Math.random() * 7);
-        var score2 = score1 == 6 ? Math.floor(Math.random() * 6) : 6;
-        var team1 = matchups[i].team1;
-        var team2 = matchups[i].team2;
+var rolling = false;
+async function randomize() {
+    if (rolling) return;
 
-        document.getElementById(`m${i + 1}-title`).innerHTML = `${team1} vs ${team2}`;
-        document.getElementById(`m${i + 1}-score`).innerHTML = `${score1} - ${score2}`;
+    document.getElementById("result").innerHTML = "Waiting...";
+    var result = ""
+
+    rolling = true;
+    var bar = document.getElementById("bar");
+    bar.style.width = "0%";
+
+    for (var j = 0; j < 100; j++) {
+        if (j >= 99) result = "**I proudly used the Suiji Pickem Randomizer to do my pickems!**<br>Here are my picks:<br><br>";
+        for (var i = 0; i < 16; i++) {
+            var score1 = Math.floor(Math.random() * 7);
+            var score2 = score1 == 6 ? Math.floor(Math.random() * 6) : 6;
+            var team1 = matchups[i].team1;
+            var team2 = matchups[i].team2;
+
+            document.getElementById(`m${i + 1}-title`).innerHTML = `${team1} vs ${team2}`;
+            document.getElementById(`m${i + 1}-score`).innerHTML = `${score1} - ${score2}`;
+
+            if (j >= 99) {
+                document.getElementById("share").innerHTML = "Share your results!";
+                if (score1 > score2) result += `**${team1} | ${score1}** - ${score2} | ${team2}<br>`;
+                else if (score1 < score2) result += `${team1} | ${score1} - **${score2} | ${team2}**<br>`;
+            }
+        }
+        await new Promise(resolve => setTimeout(resolve, Math.min(5 + Math.pow(j, 1.1)), 200));
+        bar.style.width = (j / 100) * 100 + "%";
     }
+
+    document.getElementById("result").innerHTML = result;
+    bar.style.width = "100%";
+    rolling = false;
 }
 
 const matchups = [
