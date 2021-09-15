2efd4673ef9126647fc7d667573ea2b64d79d0b2
shdewz <shdewzofficial@gmail.com>
2021-09-15 20:07:43 +0300

    Change label display

--- js/mappack.js
+++ js/mappack.js
@@ -9,11 +9,14 @@ async function generate() {
     $('#progress-error').text('');
     index = 0;
     max = ids.length + ids.length / 8;
+
     let maps = await Promise.all(ids.map(async id => ({ id: id, blob: await download_map(id) })));
     console.log(`${maps.filter(map => map.blob).length} map(s) successfully downloaded.`);
-    $('#progress-label').text(`Preparing zip file...`);
+
     let failed = maps.filter(map => !map.blob);
     if (failed.length > 0) $('#progress-error').wrapInner(`<i class="fas fa-exclamation-triangle"></i> The following maps were not downloaded:<br>${failed.map(e => `<a href="https://osu.ppy.sh/s/${e.id}">${e.id}</a>`).join(', ')}`);
+
+    $('#progress-label').text(`Preparing zip file...`);
     export_zip(maps.filter(map => map.blob));
 }
 
@@ -21,11 +24,13 @@ const download_map = async id => {
     const map = await get_map(id);
     if (map.length == 0) return false;
     console.log(`Downloading ${map.Artist} - ${map.Title} (${id})...`);
-    $('#progress-label').text(`Downloading ${map.Artist} - ${map.Title}...`);
     const url = `https://api.chimu.moe/v1/download/${id}?n=0`;
     const response = await fetch(url);
+
     index++;
+    $('#progress-label').text(`Downloading ${map.Artist} - ${map.Title}...`);
     $('#progress').css('width', Math.ceil((index / max) * 100) + '%');
+
     return response.ok ? await response.blob() : false;
 }
 
