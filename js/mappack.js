00f740f67b67d39ef1e8b061348a56d0c8768d70
shdewz <shdewzofficial@gmail.com>
2021-09-15 10:53:19 +0300

    Add mappack generator

---
+++ js/mappack.js
@@ -0,0 +1,40 @@
+let failed = [];
+let maps = [];
+
+async function generate() {
+    const ids = $('#setids').val().replace(/\s+/g, '').split(',');
+    failed, maps = [];
+    let index = 0;
+    for (const id of ids) {
+        index++;
+        if (map) maps.push({ id: id, blob: await download_map(id) });
+        else failed.push(id);
+        $('#progress').css('width', Math.ceil((index / (ids.length + ids.length / 8)) * 100) + '%');
+    }
+    console.log(`${maps.length} maps successfully downloaded.`);
+    $('#progress-label').text(`Preparing zip file...`);
+    export_zip(maps);
+}
+
+const download_map = async id => {
+    console.log(`Downloading ${id}...`);
+    $('#progress-label').text(`Downloading ${id}...`);
+    const url = `https://api.chimu.moe/v1/download/${id}?n=0`;
+    const response = await fetch(url);
+    return response.ok ? await response.blob() : false;
+}
+
+const export_zip = maps => {
+    const zip = new JSZip();
+    maps.forEach(map => zip.file(`${map.id}.osz`, map.blob));
+
+    zip.generateAsync({ type: 'blob' }).then(file => {
+        const fileName = `mappack-${ new Date().getTime()}.zip`;
+        $('#progress').css('width', '100%');
+        $('#progress-label').text(`Done`);
+        if (failed.length > 0) $('#progress-error').text(`The following maps were not downloaded:<br>${failed.join(', ')}`);
+        failed, maps = [];
+        return saveAs(file, fileName);
+        
+    });
+}
